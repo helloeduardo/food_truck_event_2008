b73f47424c3d6fe595847f5bba1523daeed233fa
Eduardo Parra <helloeduardo@icloud.com>
2020-09-15 13:04:41 -0600

    Add attributes and test

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,9 @@
 class Item
+  attr_reader :name, :price
 
   def initialize(data)
-    @data = data
+    @name = data[:name]
+    @price = data[:price]
   end
 
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -10,4 +10,11 @@ class ItemTest < Minitest::Test
     assert_instance_of Item, item1
   end
 
+  def test_it_has_attributes
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+    assert_equal 'Peach Pie (Slice)', item1.name
+    assert_equal "$3.75", item1.price
+  end
+
 end
