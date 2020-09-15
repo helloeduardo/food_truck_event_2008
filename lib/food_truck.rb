df32f769a65a5bf0c423ef69ce1bfabfc88a3519
Eduardo Parra <helloeduardo@icloud.com>
2020-09-15 13:36:46 -0600

    Add revenue method and test

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -14,4 +14,10 @@ class FoodTruck
   def stock(item, amount)
     @inventory[item] += amount
   end
+
+  def potential_revenue
+    inventory.sum do |item, amount|
+      item.price * amount
+    end
+  end
 end
